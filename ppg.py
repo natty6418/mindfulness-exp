2556a42e7bc538292b379961ecb7a9387b916e1c
natty6418 <natnaelmetekie@gmail.com>
2025-04-16 16:44:52 +0400

    fixed ppg issue

--- ppg.py
+++ ppg.py
@@ -3,6 +3,8 @@ import serial
 import sys
 from pyshimmer import ShimmerBluetooth, DEFAULT_BAUDRATE, DataPacket, EChannelType
 from pyshimmer.dev.channels import ChDataTypeAssignment, ChannelDataType, EChannelType, ESensorGroup
+import numpy as np
+from pyshimmer.util import unwrap
 
 
 
@@ -10,8 +12,11 @@ participant_id = sys.argv[1] if len(sys.argv) > 1 else "test"
 experiment_id = sys.argv[2] if len(sys.argv) > 2 else "test"
 DATA_FILE = f"./data/{experiment_id}/participant_{participant_id}_ppg_data.csv"
 
+# def unwrap_device_timestamps(ts_dev: np.ndarray) -> np.ndarray:
+#     ts_dtype = ChDataTypeAssignment[EChannelType.TIMESTAMP]
+#     return unwrap(ts_dev, 2 ** (8 * ts_dtype.size))
 
-def handler(pkt: DataPacket, file_handle, start_time_holder) -> None:
+def handler(pkt: DataPacket, file_handle) -> None:
     try:
         cur_value = pkt[EChannelType.INTERNAL_ADC_13]
         packet_timestamp = pkt[EChannelType.TIMESTAMP]        
@@ -19,25 +24,26 @@ def handler(pkt: DataPacket, file_handle, start_time_holder) -> None:
         print("Warning: PPG data not found in packet.")
         return
 
-    if start_time_holder["start_time"] is None:
-        start_time_holder["start_time"] = packet_timestamp
+    # if start_time_holder["start_time"] is None:
+    #     start_time_holder["start_time"] = packet_timestamp
 
-
-    elapsed_time = (packet_timestamp - start_time_holder["start_time"])/32768.0
-    print(f"Time: {elapsed_time:.2f}s | PPG: {cur_value}")
+    # data["timeStamp"].append(packet_timestamp)
+    # data["PPG"].append(cur_value)
+    # elapsed_time = (packet_timestamp - start_time_holder["start_time"])/DEV_CLOCK_RATE
+    print(f"Time Stamp: {packet_timestamp:.2f} | PPG: {cur_value}")
 
     # Write data to disk immediately
-    file_handle.write(f"{elapsed_time:.4f},{cur_value}\n")
+    file_handle.write(f"{packet_timestamp:.4f},{cur_value}\n")
     file_handle.flush()  # ensures data is actually written
 
 if __name__ == '__main__':
     # Open the file in write mode (or "a" if you want to append)
     with open(DATA_FILE, "w", buffering=1) as f:
-        f.write("Time(s),PPG(mV)\n")  # header line
+        f.write("TimeStamp,PPG(mV)\n")  # header line
 
         try:
             
-            serial_conn = serial.Serial('COM3', DEFAULT_BAUDRATE)
+            serial_conn = serial.Serial('COM8', DEFAULT_BAUDRATE)
             shim_dev = ShimmerBluetooth(serial_conn)
             shim_dev.initialize()
 
@@ -45,11 +51,15 @@ if __name__ == '__main__':
 
             print(f"Starting PPG data collection for Participant {participant_id}...")
             # We'll store the start time in a mutable dict so the handler can update it
-            start_time_holder = {"start_time": None}
+            # start_time_holder = {"start_time": None}
+            # data = {
+            #     "timeStamp": [],
+            #     "PPG": [],
+            # }
             
             # Add a lambda or partial so we can pass 'f' and 'start_time_holder' to the handler
             shim_dev.add_stream_callback(
-                lambda pkt: handler(pkt, f, start_time_holder)
+                lambda pkt: handler(pkt, f)
             )
             shim_dev.start_streaming()
             # status = shim_dev.get_status()
@@ -65,8 +75,17 @@ if __name__ == '__main__':
 
         except KeyboardInterrupt:
             print("\nStopping PPG data collection...")
-            shim_dev.stop_streaming()
+            # shim_dev.stop_streaming()
+            # ts_unwrapped = unwrap_device_timestamps(np.array(data["timeStamp"]))/ DEV_CLOCK_RATE
+            # # shim_dev.shutdown()
+            # for i in range(len(data["PPG"])):
+            #     f.write(f"{ts_unwrapped[i]-ts_unwrapped[0]:.4f},{data['PPG'][i]}\n")
+            # f.flush()
+            # f.close()
             # shim_dev.shutdown()
+            # file_handle.close()
+            
+            
         finally:
             print(f"\n✅ Data continuously saved to {DATA_FILE}.")
 
